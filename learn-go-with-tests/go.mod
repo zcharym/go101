e1cb1442c0816e5055ada1c92498bad4bd571bd7
Zchary-Ma <Zcharyma@gmail.com>
2021-06-03 16:19:53 +0800

    add benchmark test

---
+++ learn-go-with-tests/go.mod
@@ -0,0 +1,3 @@
+module learning_test
+
+go 1.16
--- learn-go-with-tests/iteration_test.go
+++ learn-go-with-tests/iteration_test.go
@@ -22,3 +22,9 @@ func TestRepeat(t *testing.T) {
 		}
 	})
 }
+
+func BenchmarkRepeat(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Repeat('a', 3)
+	}
+}
