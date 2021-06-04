c80e988095d5c3dc6dd432bc24c6e03fb06ff12f
Zchary-Ma <Zcharyma@gmail.com>
2021-06-04 15:39:27 +0800

    refactor:add method

--- learn-go-with-tests/type-struct-interface.go
+++ learn-go-with-tests/type-struct-interface.go
@@ -1,13 +1,25 @@
 package learning_tests
 
+import (
+	"math"
+)
+
 type Rectangle struct {
 	width, height float64
+}
+
+type Circle struct {
+	radius float64
 }
 
 func Perimeter(rect Rectangle) float64 {
 	return 2 * (rect.width + rect.height)
 }
 
-func Area(rect Rectangle) float64 {
-	return rect.width * rect.height
+func (r Rectangle) Area() float64 {
+	return r.width * r.height
+}
+
+func (c Circle) Area() float64 {
+	return c.radius * c.radius * math.Pi
 }
--- learn-go-with-tests/type-struct-interface_test.go
+++ learn-go-with-tests/type-struct-interface_test.go
@@ -1,6 +1,7 @@
 package learning_tests
 
 import (
+	"math"
 	"testing"
 )
 
@@ -14,10 +15,22 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	rect := Rectangle{10.0, 10.0}
-	got := Area(rect)
-	expected := 100.0
-	if got != expected {
-		t.Errorf("got '%.2f' want '%.2f'", got, expected)
-	}
+	t.Run("Rectangle", func(t *testing.T) {
+		rect := Rectangle{10.0, 10.0}
+		got := rect.Area()
+		expected := 100.0
+		if got != expected {
+			t.Errorf("got '%.2f' want '%.2f'", got, expected)
+		}
+	})
+
+	t.Run("Circle", func(t *testing.T) {
+		circle := Circle{10.0}
+		got := circle.Area()
+		expected := math.Pi * 100.0
+		if got != expected {
+			t.Errorf("got '%.2f' want '%.2f'", got, expected)
+		}
+	})
+
 }
