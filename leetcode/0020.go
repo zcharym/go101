e39e02dc7499d98bc0400b404b88c33f838a7fa1
Zchary-Ma <Zcharyma@gmail.com>
2021-06-08 00:07:50 +0800

    change: answer

--- leetcode/0020.go
+++ leetcode/0020.go
@@ -1,28 +1,25 @@
 package leetcode
 
-var m = map[rune]rune{
-	'}': '{',
-	']': '[',
-	')': '(',
-}
-
 func isValid(s string) bool {
-	var stack []rune
-	pushed := false
-	for _, ch := range []rune(s) {
-		// 左括号压入栈顶
-		if ch == 123 || ch == 91 || ch == 40 {
-			stack = append(stack, ch)
-			pushed = true
-		} else if (ch == 125 || ch == 93 || ch == 41) && len(stack) > 0 && m[ch] == stack[len(stack)-1] {
+	n := len(s)
+	if n%2 == 1 {
+		return false
+	}
+	pairs := map[byte]byte{
+		')': '(',
+		']': '[',
+		'}': '{',
+	}
+	var stack []byte
+	for i := 0; i < n; i++ {
+		if pairs[s[i]] > 0 {
+			if len(stack) == 0 || stack[len(stack)-1] != pairs[s[i]] {
+				return false
+			}
 			stack = stack[:len(stack)-1]
 		} else {
-			// 单个右括号无匹配
-			return false
+			stack = append(stack, s[i])
 		}
 	}
-	if len(stack) == 0 && pushed == true {
-		return true
-	}
-	return false
+	return len(stack) == 0
 }
