d1cb3f0115a0d744e849b8899c29c5526da11652
Zchary-Ma <Zcharyma@gmail.com>
2021-07-13 13:00:08 +0800

    feat(intro):go goroutine chan

--- intro/sleepygophers.go
+++ intro/sleepygophers.go
@@ -6,13 +6,20 @@ import (
 )
 
 func main() {
+	c := make(chan int)
 	for i := 0; i < 5; i++ {
-		go sleepyGophers() // goroutine orders execute randomly
+		go sleepyGophers(i, c) // goroutine orders execute randomly
 	}
 	time.Sleep(time.Second * 4) // -> use chan to receive stop signal
+
+	for j := 0; j < 5; j++ {
+		gopherId := <-c
+		fmt.Println("gopher", gopherId, "finished sleeping.")
+	}
 }
 
-func sleepyGophers() {
+func sleepyGophers(id int, c chan int) {
 	time.Sleep(time.Second * 3)
-	fmt.Println("...snore...")
+	fmt.Printf("...[%d]snore...\n", id)
+	c <- id
 }
