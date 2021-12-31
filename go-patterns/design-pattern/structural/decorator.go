a58edcaf6a2cabee35f185988965b75278ee7f21
Zchary-Ma <Zcharyma@gmail.com>
2021-12-31 21:08:02 +0800

    update(pattern): decorator pattern

--- go-patterns/design-pattern/structural/decorator.go
+++ go-patterns/design-pattern/structural/decorator.go
@@ -1,42 +1,16 @@
-package main
+package structural
 
 import (
-	"log"
-	"os"
-	"time"
+    "log"
+    "time"
 )
 
-// type fiboFunc func() func() int
-
-// TODO import from module
-func fibonacci() func() int {
-	first, second := 0, 1
-	return func() int {
-		ret := first
-		first, second = second, first+second
-		return ret
-	}
-}
-
-type sumFunc func([]int) int
-
-func sum(nums []int) (total int) {
-	for _, num := range nums {
-		total += num
-	}
-	return
-}
-
-func logger(fn sumFunc, logger *log.Logger) sumFunc {
-	return func(nums []int) int {
-		log.Printf("time:%v", time.Now())
-		return fn(nums)
-	}
-}
-
-func main() {
-	nums := []int{1, 2, 3}
-	f := logger(sum, log.New(os.Stdout, "test", 1))
-
-	f(nums)
+func decorator(fn func(s string)) func(s string) {
+    return func(s string) {
+        start := time.Now()
+        time.Sleep(time.Millisecond * 500) // mimic func execution
+        fn(s)
+        elapsed := time.Since(start)
+        log.Printf("func execution time is: %dms", elapsed / time.Millisecond)
+    }
 }
